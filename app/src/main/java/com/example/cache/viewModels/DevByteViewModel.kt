1eb09c2b98d2df11cc058a9ce768e52ee5f64ade
Amanda Soares <amanda.soares@ifood.com.br>
2024-04-02 20:07:46 -0300

    incluindo repository no viewmodel

--- app/src/main/java/com/example/cache/viewModels/DevByteViewModel.kt
+++ app/src/main/java/com/example/cache/viewModels/DevByteViewModel.kt
@@ -7,58 +7,28 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
+import com.example.cache.database.getDatabase
 import com.example.cache.domain.Video
 import com.example.cache.network.Network
 import com.example.cache.network.asDomainModel
+import com.example.cache.repository.VideosRepository
 import kotlinx.coroutines.launch
 import java.io.IOException
 
 class DevByteViewModel(application: Application) : AndroidViewModel(application) {
 
-    /**
-     *
-     */
+    private val database =  getDatabase(application)
+    private val videosRepository = VideosRepository(database)
 
-    /**
-     *
-     */
+     var playlist : LiveData<List<Video>>
 
-    /**
-     * A playlist of videos that can be shown on the screen. This is private to avoid exposing a
-     * way to set this value to observers.
-     */
-    private val _playlist = MutableLiveData<List<Video>>()
-
-    /**
-     * A playlist of videos that can be shown on the screen. Views should use this to get access
-     * to the data.
-     */
-    val playlist: LiveData<List<Video>>
-        get() = _playlist
-
-    /**
-     * init{} is called immediately when this ViewModel is created.
-     */
     init {
-        refreshDataFromNetwork()
-    }
-
-    /**
-     * Refresh data from network and pass it via LiveData. Use a coroutine launch to get to
-     * background thread.
-     */
-    private fun refreshDataFromNetwork() = viewModelScope.launch {
-        try {
-            val playlist = Network.devbytes.getPlaylist().await()
-            _playlist.postValue(playlist.asDomainModel())
-        } catch (networkError: IOException) {
-            // Show an infinite loading spinner if the request fails
-            // challenge exercise: show an error to the user if the network request fails
+        viewModelScope.launch {
+            videosRepository.refreshVideos()
         }
-    }
 
-    /**
-     */
+         playlist = videosRepository.videos
+    }
 
     /**
      * Factory for constructing DevByteViewModel with parameter
